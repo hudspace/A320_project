713882ae911df5434ea35cc028994ddb97e92c50
Brian Hudson <hudson_ops@brians-imac.attlocal.net>
2018-08-16 19:41:02 -0400

    Shuffled and renamed some files...now working with A320LP as project name, Vappwf as first app name. Vappwof deleted for now, pending addition later.

--- Vappwf/views.py
+++ Vappwf/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page(self):
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>A320 Landing Performance Web Application</title></html>')
 
